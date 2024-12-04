c9af342c48bb32b754a158865de6a16b727e3517
Fungus <jkfungus@yandex.com>
2024-12-04 21:08:29 +0800

    Update to 1.21.4

--- src/main/java/dev/dubhe/gugle/carpet/mixin/SlotMixin.java
+++ src/main/java/dev/dubhe/gugle/carpet/mixin/SlotMixin.java
@@ -1,9 +1,7 @@
 package dev.dubhe.gugle.carpet.mixin;
 
-import com.mojang.datafixers.util.Pair;
 import dev.dubhe.gugle.carpet.tools.SlotIcon;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.inventory.Slot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -15,19 +13,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Slot.class)
 public class SlotMixin implements SlotIcon {
     @Unique
-    private Pair<ResourceLocation, ResourceLocation> pair;
+    private ResourceLocation noItemIcon = null;
 
     @Inject(method = "getNoItemIcon", at = @At("HEAD"), cancellable = true)
-    private void getNoItemIcon(CallbackInfoReturnable<Pair<ResourceLocation, ResourceLocation>> cir) {
-        if (this.pair != null) {
-            cir.setReturnValue(this.pair);
+    private void getNoItemIcon(CallbackInfoReturnable<ResourceLocation> cir) {
+        if (this.noItemIcon != null) {
+            cir.setReturnValue(this.noItemIcon);
         }
     }
 
     @Override
     public void setIcon(ResourceLocation resource) {
         if (resource != null) {
-            this.pair = Pair.of(InventoryMenu.BLOCK_ATLAS, resource);
+            this.noItemIcon = resource;
         }
     }
 }
